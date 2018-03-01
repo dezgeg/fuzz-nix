1b83886f108afdd511799e94503f59605cf2ecd7
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-03-01 22:08:46 +0200

    Fix tmin diff

--- tmin.sh
+++ tmin.sh
@@ -18,4 +18,4 @@ mkdir -p minimized-tmin
 for f in $inputs; do
     $AFL/bin/afl-tmin -i "$f" -o minimized-tmin/"$(basename "$f")" -m 300 result/bin/nix-instantiate --eval --strict --option restrict-eval true --dry-run @@ || break
 done
-vimdiff <(grep '.*' $inputs) <(cd minimized-tmin && grep '.*' *)
+vimdiff <(grep '.*' $inputs | perl -pe 's|(?:[^:]+/)?([^:]+):|$1:|') <(cd minimized-tmin && grep '.*' *)
