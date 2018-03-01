c28d15bdc43ac2c8d57d2b16e2cc572a24235c4e
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-03-01 22:10:49 +0200

    Don't rm findings

--- fuzz.sh
+++ fuzz.sh
@@ -2,6 +2,4 @@
 
 source common.sh
 
-rm -rf findings
-
 $AFL/bin/afl-fuzz -i inputs/corpus -x inputs/dict -o findings -m 300 result/bin/nix-instantiate --eval --strict --option restrict-eval true --dry-run @@
