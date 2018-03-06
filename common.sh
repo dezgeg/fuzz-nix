c3c643b107e049becc61d5eefe5a77992fcfc53b
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-03-06 02:37:01 +0200

    Reorganize some tokens

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 afl=$(nix-build --no-out-link '<nixpkgs>' -A afl)
 aflNix=$(nix-build --no-out-link -A aflBuild)
 
-export NIX_PATH=foo=$(readlink -f .)
+export NIX_PATH=r=$(readlink -f .)
 export AFL_SKIP_CPUFREQ=1
 export GC_INITIAL_HEAP_SIZE=$(( 8 * 1024 * 1024 ))
