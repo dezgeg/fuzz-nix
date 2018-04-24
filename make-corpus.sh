41628998ccd306dd96c2850498862108adf569fe
Tuomas Tynkkynen <tuomas@tuxera.com>
2018-04-24 04:08:52 +0300

    make-corpus.sh: Use content-addressable filenames
    This is so that adding a new input/keyword in the middle of others
    doesn't shift the filenames of existing inputs/keywords.

--- make-corpus.sh
+++ make-corpus.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+checksum() {
+    echo -n "$@" | sha256sum | awk '{print $1}'
+}
+
 if [ "$#" -gt 0 ]; then
     exprs="$@"
 else
@@ -10,23 +14,17 @@ mkdir -p inputs
 rm -rf inputs/corpus
 mkdir -p inputs/corpus
 
-i=1
 cat $exprs | while read -r line; do
     if [ -z "$line" ]; then continue; fi
-    n=$(printf "%03d" $i)
-    echo "$line" > inputs/corpus/initial-$n.in
-    (( i++ ))
+    echo "$line" > inputs/corpus/initial-$(checksum "$line").in
 done
 
 rm -rf inputs/dict
 mkdir -p inputs/dict
 
-i=1
 cat dict/*.txt | while read -r line; do
     if [ -z "$line" ]; then continue; fi
-    n=$(printf "%03d" $i)
-    echo "$line" > inputs/dict/$n.kw
-    (( i++ ))
+    echo "$line" > inputs/dict/$(checksum "$line").kw
 done
 
 echo -e ' ' > inputs/dict/space.txt
